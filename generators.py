f7c50789d999cff7e30356b18cc2d600bfd8cd18
pawtul <pawtul@gmail.com>
2017-01-16 01:12:18 -0800

    Added generating data files

--- generators.py
+++ generators.py
@@ -1,3 +1,5 @@
+import argparse
+
 from random import randint
 
 import numpy as np
@@ -13,9 +15,21 @@ def generate_random_matrix(n):
 def generate_files_with_matrix(n, filename):
     mat = generate_random_matrix(n)
     cplex_mat = mat + np.eye(n)
-    with open(filename+'.mod') as file:
+    for i in range(n):
+        if not sum(mat[i]):
+            return generate_files_with_matrix(n, filename)
+
+    with open(filename+'.mod', "w") as file:
         file.write("mat = " + str(cplex_mat) + ";")
 
-    with open(filename+'.py') as file:
+    with open(filename+'.py', "w") as file:
         file.write("mat = " + str(mat))
+
+parser = argparse.ArgumentParser("Generates file of matrices")
+parser.add_argument("-n", dest='n', type=int)
+parser.add_argument("-f", dest="f")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    generate_files_with_matrix(args.n, args.f)
 
